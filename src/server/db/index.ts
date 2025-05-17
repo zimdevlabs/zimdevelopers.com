4f4c3a9b201b017743d25e6fa2dc41b9ed9561e1
Tawanda Chiteshe <dickenstawanda@gmail.com>
2025-05-17 21:27:11 +0200

    feat: add node postgres db

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,7 +4,12 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 import * as schema from "./schema";
-
+import { drizzle as nodedrizzle } from "drizzle-orm/node-postgres"
 config({ path: ".env.local" });
 
-export const db = drizzle(neon(process.env.DATABASE_URL!), { schema });
+const neondb = drizzle(neon(process.env.DATABASE_URL!), { schema });
+const postgresdb = nodedrizzle(process.env.DATABASE_URL!, { schema });
+
+const db = process.env.NODE_ENV === "development" ? postgresdb : neondb;
+
+export { db };
